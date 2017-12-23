a2d88c0eb58a4d9bda230c0964edb7f20554335b
SaviorD7 <savior.7@yandex.ru>
2017-12-24 01:41:32 +0300

    New design

---
+++ tasks/det_4/js/scrollup.js
@@ -0,0 +1,15 @@
+jQuery( document ).ready(function() {
+	var top_show = 150;
+	var delay = 500;
+	$(document).ready(function() {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > top_show) $('div#independent-block').fadeIn();
+			else $('div#independent-block').fadeOut();
+		});
+		$('div#scrollup').click(function () {
+			$('body, html').animate({
+				scrollTop: 0
+			}, delay);
+		});
+	});
+});
